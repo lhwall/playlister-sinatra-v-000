8fbc12ce32b634837c6eea7e6f0ff2709c225682
Lucy Wall <lhwall@gmail.com>
2018-06-20 23:32:09 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,26 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def slug
+    slug = ""
     #binding.pry
+    self.name.to_s.split("").each do |character|
+      if character.match(/[a-zA-Z]/)
+        slug << character.downcase
+      elsif character.match(/\s/)
+        slug << "-"
+      end
+    end
+    slug
+  end
+
+  def self.find_by_slug(entry)
+    value = nil
+    self.all.each do |artist|
+      if artist.slug == entry
+        value = artist
+      end
+    end
+    value
   end
 
 end
