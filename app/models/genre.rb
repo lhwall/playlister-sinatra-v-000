53acb68760b189308547f8db976b28730453aadc
Lucy Wall <lhwall@gmail.com>
2018-06-20 23:38:02 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,30 @@
 class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :artist_genres
-  has_many :songs, through: :song_genre
+  has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+  def slug
+    slug = ""
+    #binding.pry
+    self.name.to_s.split("").each do |character|
+      if character.match(/[a-zA-Z]/)
+        slug << character.downcase
+      elsif character.match(/\s/)
+        slug << "-"
+      end
+    end
+    slug
+  end
+
+  def self.find_by_slug(entry)
+    value = nil
+    self.all.each do |genre|
+      if genre.slug == entry
+        value = genre
+      end
+    end
+    value
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,28 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
   belongs_to :artist
+
+  def slug
+    slug = ""
+    #binding.pry
+    self.name.to_s.split("").each do |character|
+      if character.match(/[a-zA-Z]/)
+        slug << character.downcase
+      elsif character.match(/\s/)
+        slug << "-"
+      end
+    end
+    slug
+  end
+
+  def self.find_by_slug(entry)
+    value = nil
+    self.all.each do |song|
+      if song.slug == entry
+        value = song
+      end
+    end
+    value
+  end
+
 end
