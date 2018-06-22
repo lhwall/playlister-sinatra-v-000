d6945007ea829bc08c16c74b58e2b8dba7406fd6
Lucy Wall <lhwall@gmail.com>
2018-06-22 01:27:19 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,24 @@
+class SongsController < ApplicationController
+
+  get "/songs" do
+    @songs = Song.all
+    erb :"/songs/index"
+  end
+
+  get "/songs/new" do
+    erb :"/songs/new"
+  end
+
+ post "/songs" do
+   @song = Song.create(params[:song])
+
+   redirect to "/songs/#{song.id}"
+ end
+
+ get "/songs/:id" do
+   @song = Song.find(params[:id])
+   erb :"/songs/show"
+ end
+
+
+end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,8 +1,8 @@
+require_relative './concerns/slugafiable.rb'
+
 class Artist < ActiveRecord::Base
   extend Slugafiable::ClassMethods
   include Slugafiable::InstanceMethods
   has_many :songs
   has_many :genres, through: :songs
-
-
 end
