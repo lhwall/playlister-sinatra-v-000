84b0a6528016f293463c02f28a3dcdc7a7ed14f9
Lucy Wall <lhwall@gmail.com>
2018-06-23 22:59:05 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
    if @song.artist
    @artist = @song.artist
  end
- if song.genres
+ if @song.genres
    @genres = @song.genres
  end
    erb :"/songs/show"
