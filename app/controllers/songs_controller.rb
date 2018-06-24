caa563158b73ee152f827851b05c6352a21405f2
Lucy Wall <lhwall@gmail.com>
2018-06-24 00:18:26 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,6 @@ class SongsController < ApplicationController
 #binding.pry
    @song = Song.create(:name => params[:Name])
    @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])  #  else
-   if params[:genres]
    @song.genre_ids << params[:genres][]
  end
    @song.save
